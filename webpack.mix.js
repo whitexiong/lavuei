4447904acd61d251f30faba982d1ba5895d921d6
whitexiong <986247535@qq.com>
2021-11-14 12:44:12 +0800

    创建 sail 开发环境

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,9 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+
+mix.browserSync({
+   proxy: 'http://localhost:8000',
+   open: false,
+});
